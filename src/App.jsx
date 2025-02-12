cb1e2d7144050bfe2046ec2b2e665c3fc0f14dce
aya-asylbek <aichurek0990@gmail.com>
2025-02-12 00:43:46 -0800

    Imported App.css file to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import Game from "./Game";
+import Game from "./Game";{/*  Import Game file */}
+import "./App.css";{/*  Import  CSS file */}
 
 function App() {
    return (
--- src/Game.jsx
+++ src/Game.jsx
@@ -4,7 +4,7 @@ function Game(){
     return (
         <div className="game-area">
             <header className="game-header">
-                <h2 className="game-geader-title">Guess a number between 1 and 100</h2>
+                <h2 className="game-header-title">Guess a number between 1 and 100</h2>
             <button>New Game</button>
             </header>
             </div>
