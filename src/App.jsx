da4ea4e76de5d85bdbab6454700811831a165ebe
aya-asylbek <aichurek0990@gmail.com>
2025-02-12 00:27:02 -0800

    Added button New game and a header of button Guess a number between 1 and 100

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { useState } from 'react'
-
-import './App.css'
+import React from "react";
+import Game from "./Game";
 
 function App() {
    return (
     <>
-    <h1>Guess My Number!!!</h1>
+    <h1>Guess My Number</h1>
+    <Game />
       </>
   )
 }
--- src/Game.jsx
+++ src/Game.jsx
@@ -1,2 +1,14 @@
 import React from 'react';
 
+function Game(){
+    return (
+        <div className="game-area">
+            <header className="game-header">
+                <h2 className="game-geader-title">Guess a number between 1 and 100</h2>
+            <button>New Game</button>
+            </header>
+            </div>
+    );
+}
+
+export default Game;
