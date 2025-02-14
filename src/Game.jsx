96c4563af6b1bb979d44cfd37dd0a8d0e81e8237
aya-asylbek <aichurek0990@gmail.com>
2025-02-13 21:47:38 -0800

    Start or restart the game with startNewGame function

--- src/Game.jsx
+++ src/Game.jsx
@@ -1,24 +1,38 @@
-import React,{ useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 
-function Game(props){
-    const [targetNumber,setTargetNumber] = useState(null);
-    const [currentGuess,setCurrentGuess] = useState('');
-    const [attemptsLeft,setAttemptLeft] = useState(10);
-    const [message ,setMessage] =  useState('');
-    const [gameWon ,setGameWon] = useState(false);
-    const [gameeLost,setGameLost]= useState(false);
+function Game(props) {
+    const [targetNumber, setTargetNumber] = useState(null);
+    const [currentGuess, setCurrentGuess] = useState('');
+    const [attemptsLeft, setAttemptsLeft] = useState(10);
+    const [message, setMessage] = useState('');
+    const [gameWon, setGameWon] = useState(false);
+    const [gameLost, setGameLost] = useState(false);
+
     
+// Function to generate  a random number//
+    const generateNumber = () => Math.floor(Math.random() * 100) + 1;
+
+// Start or restart the game
+    const startNewGame = () => {
+        setTargetNumber(generateNumber());
+        setAttemptsLeft(10);
+        setCurrentGuess('');
+        setGameWon(false);
+        setGameLost(false);
+        setMessage('');
+    };
+
     //Initialize game
     useEffect(() => {
         startNewGame();
-    } [];
-    
-    
+    },[]);
+
+
     return (
         <div className="game-area">
             <header className="game-header">
                 <h2 className="game-header-title">{props.title}</h2>
-            <button>New Game</button>
+                <button>New Game</button>
             </header>
 
             <section className="game-body">
@@ -27,16 +41,16 @@ function Game(props){
                     <br /> {`Good luck!`}
                 </h2>
 
-                
-                
-                 </section>
+
+
+            </section>
 
 
 
 
 
 
-            </div>
+        </div>
     );
 }
 
