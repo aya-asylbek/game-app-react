3b5b6d74053fae33a1ce6addbe5cbebb71872404
aya-asylbek <aichurek0990@gmail.com>
2025-02-14 00:19:02 -0800

    Added updated background

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,10 @@ import Game from "./Game";
 import "./App.css";
 
 function App() {
-   return (
+  return (
     <>
-    
-    <Game title="Guess a number between 1 and 100" />
-      </>
+      <Game title="Guess a number between 1 and 100!" />
+    </>
   )
 }
 export default App
